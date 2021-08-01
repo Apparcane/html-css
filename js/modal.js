2f832351181cf4f21d9d61a0d84ed6f839b8d7b2
BurnDaniil <brodiyla4@gmail.com>
2021-08-01 16:29:08 +0300

    add mobile menu

--- js/modal.js
+++ js/modal.js
@@ -7,7 +7,8 @@ const refs = {
       refs.openModalBtn.addEventListener("click", toggleModal);
       refs.closeModalBtn.addEventListener("click", toggleModal);
 
-      function toggleModal() {
+function toggleModal() {
+        document.body.classList.toggle("modal-open")
         refs.backdrop.classList.toggle("is-hidden");
       }
 
